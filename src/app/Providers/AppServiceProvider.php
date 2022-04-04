f15b555b7a787c9e738dd7e27686100491920cf2
keijiro-okamura <keijiro.okamura@excite.jp>
2022-04-04 18:26:18 +0900

    [add]Horoscopesモジュールをサービスコンテナに登録

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Modules\HoroscopesApi;
+use App\Modules\HoroscopesInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(HoroscopesInterface::class, HoroscopesApi::class);
     }
 
     /**
